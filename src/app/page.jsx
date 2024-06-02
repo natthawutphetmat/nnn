c6ad9f60940bd8f2879af150f8d8360f8cd24dfa
natthawutphetmat <tanakorn84836@gmail.com>
2024-06-02 11:50:16 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,44 @@
+
+
+import React from 'react';
+import Header from './Home/header';
+import Main from './Home/main';
+import Head from 'next/head';
+
+
+
+
+
+export default function Page() {
+  
+  return (
+
+
+<>
+
+
+
+  
+<Header/> 
+<header className="masthead">
+            <div className="box">
+            <a href="https://lin.ee/g1H4k9G" target='bank'> 
+              <img src="/imgs/1 (1).jpg" width="100%" alt="pgslot" />
+<img src="/imgs/btn1.gif" width="100%" alt="pgslot" />
+<img src="/imgs/1 (2).jpg" width="100%" alt="pgslot" />
+<img src="/imgs/btn2.gif" width="100%" alt="pgslot" />
+<img src="/imgs/1 (3).jpg" width="100%" alt="pgslot" />
+<img src="/imgs/btn3.gif" width="100%" alt="pgslot" />
+<img src="/imgs/1 (4).jpg" width="100%" alt="pgslot" />
+<img src="/imgs/btn1.gif" width="100%" alt="pgslot" />
+<img src="/imgs/1 (5).jpg" width="100%" alt="pgslot" />
+<img src="/imgs/btn1.gif" width="100%" alt="pgslot" />
+<img src="/imgs/1 (6).jpg" width="100%" alt="pgslot" />
+              </a>
+            </div>
+        </header>
+<Main/> 
+    
+</>
+  );
+}
